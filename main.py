83751a233162fab397c410bb1b781dbc2c3bbb07
Uwe Kamper <me@uwekamper.de>
2023-11-20 22:57:22 +0100

    1st working version

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from authlib.integrations.starlette_client import OAuth, OAuthError
 from contextlib import asynccontextmanager
 from starlette.config import Config
-from fastapi import Depends, FastAPI, Request, BackgroundTasks
+from fastapi import Depends, FastAPI, Request, Form
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi import BackgroundTasks
@@ -13,12 +13,19 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 from starlette.middleware.sessions import SessionMiddleware
 # from fastapi.security import OAuth2AuthorizationCodeBearer
 from starlette.responses import HTMLResponse, RedirectResponse
+from sqlalchemy import create_engine, Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.sql import func
+
 
 class Settings(BaseSettings):
     client_id: str
     client_secret: str
     session_secret: str
-
+    database_url: str
+    voucher: str
+    total_tickets: int
     # File '.env' will be read
     model_config = SettingsConfigDict(env_file=".env")
 
@@ -40,7 +47,6 @@ oauth.register(
     }
 )
 
-
 # @asynccontextmanager
 # async def lifespan(app: FastAPI):
 #     # Load the ML model
@@ -57,6 +63,24 @@ app = FastAPI()
 # oauth2_scheme = OAuth2AuthorizationCodeBearer(scopes={"openid": "openid"}, authorizationUrl="https://c-base.org/oauth/authorize/", tokenUrl="https://c-base.org/oauth/token/")
 app.add_middleware(SessionMiddleware, secret_key=settings.session_secret)
 
+engine = create_engine(
+    settings.database_url, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nickname = Column(String, unique=True, index=True)
+    num_tickets = Column(String)
+
+# create sql tables
+Base.metadata.create_all(engine)
+
 
 # Static files
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -66,12 +90,53 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 async def homepage(request: Request):
     user = request.session.get('user')
     if user:
-        context = {
-            "data": json.dumps(user),
-            "user": user,
-            "request": request,
-        }
-        return templates.TemplateResponse("index.html", context)
+        with Session(engine) as db:
+            users = list(db.query(User).filter(User.num_tickets != 0))
+            sum_tickets = db.query(func.sum(User.num_tickets).label("total_score"))[0][0]
+            if sum_tickets is None:
+                sum_tickets = 0
+            tickets_left = settings.total_tickets - sum_tickets
+            
+            context = {
+                "data": json.dumps(user),
+                "user": user,
+                "users": users,
+                "request": request,
+                "sum_tickets": sum_tickets,
+                "tickets_left": tickets_left,
+                "total_tickets": settings.total_tickets
+            }
+            return templates.TemplateResponse("index.html", context)
+    return templates.TemplateResponse("index_login_required.html", {"request": request})
+
+
+@app.post('/giveme/')
+async def update_ticket(request: Request, 
+                        num_tickets: Annotated[int, Form()], 
+                        agree1: Annotated[bool, Form()]=False,
+                        agree2: Annotated[bool, Form()]=False):
+    user = request.session.get('user')
+    if user:
+        if agree1 is False or agree2 is False:
+            return templates.TemplateResponse("givemeno.html", {"request": request, "user": user})
+
+        with Session(engine) as db:
+            query = list(db.query(User).filter(User.nickname == user['nickname']))
+            if len(query) == 0:
+                new_entry = User(
+                    nickname=user['nickname'],
+                    num_tickets=num_tickets,
+                )
+                db.add(new_entry)
+                db.commit()
+            else:
+                my_nick = query[0]
+                my_nick.num_tickets = num_tickets
+                db.add(my_nick)
+                db.commit()
+                pass
+            return templates.TemplateResponse("giveme.html", {"request": request, "user": user, "voucher": settings.voucher})
+
     return templates.TemplateResponse("index_login_required.html", {"request": request})
 
 
